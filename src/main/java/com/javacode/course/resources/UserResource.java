8f5ca4ee81512476b041f02bba5754d5f4bd8b1d
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-10 12:30:10 -0300

    feat: user entity and resource

---
+++ src/main/java/com/javacode/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.javacode.course.resources;
+
+import com.javacode.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> getAll() {
+        User u = new User(1L, "João", "gmail", "558599999999", "joao123");
+
+        return ResponseEntity.ok().body(u);
+    }
+}
