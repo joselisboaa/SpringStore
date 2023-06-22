72363728233a0187ac1a8c660381b89f749e9178
joselisboaa <joselisboaa05@gmail.com>
2023-06-22 10:51:54 -0300

    fix: receive message in arguments

--- src/main/java/com/javacode/course/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/javacode/course/services/exceptions/ResourceNotFoundException.java
@@ -2,7 +2,7 @@ package com.javacode.course.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
     private static final long serialVersionUID = 1L;
-    public ResourceNotFoundException(Object id) {
-        super("Resource not found. Id " + id);
+    public ResourceNotFoundException(String message) {
+        super(message);
     }
 }
