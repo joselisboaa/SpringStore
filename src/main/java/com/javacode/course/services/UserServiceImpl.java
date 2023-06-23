fb90f16ce4a395c47e7aa17840dd2cb40ba7245d
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-23 11:13:59 -0300

    feat: implements user service interface

--- src/main/java/com/javacode/course/services/UserServiceImpl.java
+++ src/main/java/com/javacode/course/services/UserServiceImpl.java
@@ -8,34 +8,37 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserServiceImpl implements IUserService {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Override
     public List<User> getAll() {
         return userRepository.findAll();
     }
 
+    @Override
     public User getById(Long id) {
         Optional<User> user = userRepository.findById(id);
 
         return user.orElseThrow(() -> new ResourceNotFoundException(("User not found or not exists.")));
     }
 
+    @Override
     public User create(User user) {
         User createdUser = userRepository.save(user);
 
         return createdUser;
     }
 
+    @Override
     public void delete(Long id) {
         try {
             userRepository.deleteById(id);
@@ -46,6 +49,7 @@ public class UserService {
         }
     }
 
+    @Override
     public User update(Long id, User user) {
         try {
             User previousUserData = userRepository.getReferenceById(id);
