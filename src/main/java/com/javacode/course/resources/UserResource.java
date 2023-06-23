7b30825a748b163f6d021e15d6b27ae065237be9
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-23 14:59:10 -0300

    feat: use DTO

--- src/main/java/com/javacode/course/resources/UserResource.java
+++ src/main/java/com/javacode/course/resources/UserResource.java
@@ -1,9 +1,11 @@
 package com.javacode.course.resources;
 
+import com.javacode.course.dto.UserDTO;
 import com.javacode.course.entities.User;
 import com.javacode.course.services.IUserService;
 import com.javacode.course.services.UserServiceImpl;
 import jakarta.annotation.Resource;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users/")
@@ -19,31 +22,42 @@ import java.util.List;
 public class UserResource {
 
     @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
     private IUserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> getAll() {
+    public ResponseEntity<List<UserDTO>> getAll() {
         List<User> userList = service.getAll();
-        return ResponseEntity.ok().body(userList);
+        List<UserDTO> userDTOList = userList.stream()
+                .map(user -> modelMapper.map(user, UserDTO.class)).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(userDTOList);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> getById(@PathVariable Long id) {
+    public ResponseEntity<UserDTO> getById(@PathVariable Long id) {
         try {
             User user = service.getById(id);
-            return ResponseEntity.ok().body(user);
+            UserDTO userDTO = modelMapper.map(user, UserDTO.class);
+
+            return ResponseEntity.ok().body(userDTO);
         } catch (ResponseStatusException error) {
             return ResponseEntity.notFound().build();
         }
     }
 
     @PostMapping
-    public ResponseEntity<User> create(@RequestBody User user) {
+    public ResponseEntity<UserDTO> create(@RequestBody User user) {
         User createdUser = service.create(user);
+
+        UserDTO userDTO = modelMapper.map(createdUser, UserDTO.class);
+
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(createdUser.getId()).toUri();
 
-        return ResponseEntity.created(uri).body(createdUser);
+        return ResponseEntity.created(uri).body(userDTO);
     }
 
     @DeleteMapping(value = "/{id}")
@@ -54,9 +68,10 @@ public class UserResource {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<User> put(@PathVariable Long id, @RequestBody User user) {
+    public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody User user) {
         User updatedUser = service.update(id, user);
+        UserDTO userDTO = modelMapper.map(updatedUser, UserDTO.class);
 
-        return ResponseEntity.ok().body(updatedUser);
+        return ResponseEntity.ok().body(userDTO);
     }
 }
