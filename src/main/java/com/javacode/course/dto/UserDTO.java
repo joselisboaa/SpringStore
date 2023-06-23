b13d7304f3ffe0a0c6c091f0b1b017a09f7fd43f
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-23 14:38:38 -0300

    feat: user DTO

---
+++ src/main/java/com/javacode/course/dto/UserDTO.java
@@ -0,0 +1,68 @@
+package com.javacode.course.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Objects;
+
+public class UserDTO {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+
+    @NotBlank(message = "The email cannot be blank")
+    @Email
+    private String email;
+
+    @NotBlank(message = "The number cannot be blank")
+    private String number;
+
+    @NotBlank(message = "The name cannot be blank")
+    private String name;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDTO userDTO = (UserDTO) o;
+        return Objects.equals(id, userDTO.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
