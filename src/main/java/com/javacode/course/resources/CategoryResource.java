69a7f276fd3372673f4e75a5629a6b14651348fd
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-23 15:31:02 -0300

    feat: fit resource to category dto

--- src/main/java/com/javacode/course/resources/CategoryResource.java
+++ src/main/java/com/javacode/course/resources/CategoryResource.java
@@ -1,9 +1,12 @@
 package com.javacode.course.resources;
 
+import com.javacode.course.dto.CategoryDTO;
 import com.javacode.course.entities.Category;
 import com.javacode.course.services.CategoryServiceImpl;
 import com.javacode.course.services.ICategoryService;
 import jakarta.annotation.Resource;
+import jakarta.persistence.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,33 +14,44 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Resource
 @RestController
 @RequestMapping("/categories/")
 public class CategoryResource {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Autowired
     private ICategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<Category>> getAll() {
+    public ResponseEntity<List<CategoryDTO>> getAll() {
         List<Category> categories = service.getAll();
-        return ResponseEntity.ok().body(categories);
+        List<CategoryDTO> categoryDTOList = categories.stream()
+                .map(category -> modelMapper.map(category, CategoryDTO.class)).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(categoryDTOList);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Category> getById(@PathVariable Long id) {
+    public ResponseEntity<CategoryDTO> getById(@PathVariable Long id) {
         Category category = service.getById(id);
-        return ResponseEntity.ok().body(category);
+        CategoryDTO categoryDTO = modelMapper.map(category, CategoryDTO.class);
+
+        return ResponseEntity.ok().body(categoryDTO);
     }
 
     @PostMapping
-    public ResponseEntity<Category> create(@RequestBody Category category) {
+    public ResponseEntity<CategoryDTO> create(@RequestBody Category category) {
         Category createdCategory = service.create(category);
+        CategoryDTO createdCategoryDTO = modelMapper.map(createdCategory, CategoryDTO.class);
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdCategory.getId()).toUri();
 
-        return ResponseEntity.created(uri).body(createdCategory);
+        return ResponseEntity.created(uri).body(createdCategoryDTO);
     }
 
     @DeleteMapping(value = "/{id}")
@@ -48,10 +62,11 @@ public class CategoryResource {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category newCategoryData) {
+    public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody Category newCategoryData) {
         Category updatedCategory = service.update(id, newCategoryData);
+        CategoryDTO updatedCategoryDTO = modelMapper.map(updatedCategory, CategoryDTO.class);
 
-        return ResponseEntity.ok().body(updatedCategory);
+        return ResponseEntity.ok().body(updatedCategoryDTO);
     }
 }
 
